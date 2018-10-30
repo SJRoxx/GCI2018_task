2ea09a5c76e990daffe12fb21d349f63b780eabd
Shrish Jaiswal <44002551+SJRoxx@users.noreply.github.com>
2018-10-31 02:35:17 +0530

    Add files via upload

---
+++ gci2018.py
@@ -0,0 +1,10 @@
+for i in range (10):
+  print ("GCI is great")
+
+name = input ('Enter your name: ')
+
+print ("Hello,", name, "please to meet you!")
+
+n1=(name[::-1])
+
+print("Hello",name, "please to meet you! Did you know that your name backwards is",n1,"?")
